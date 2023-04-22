00956933c1345ca8825acaf57af13c9f7dec784f
ajmccredie <127118760+ajmccredie@users.noreply.github.com>
2023-04-22 21:15:03 +0000

    Add text free logo to home page and make header responsive for smaller screens

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,13 +2,13 @@ const lineContainer = document.getElementsByClassName("line-container");
 const menuBox = document.getElementsByClassName("menu-box");
 
 lineContainer.addEventListener("click", function() {
-   // lineContainer.classList.toggle("active")
-   // menuBox.classList.toggle("active")
-    if (this.classList.contains("active")) {
-        this.classList.remove("active");
-      } else this.classList.add("active");
+    lineContainer.classList.toggle("active")
+    menuBox.classList.toggle("active")
+   // if (this.classList.contains("active")) {
+     //   this.classList.remove("active");
+     // } else this.classList.add("active");
     });
-})
+
 
 //document.getElementById("myDiv")
 //.addEventListener("click", function() {
