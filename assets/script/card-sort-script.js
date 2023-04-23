79679418a24eeec5d1edeb7a1c776c70221f00b7
ajmccredie <127118760+ajmccredie@users.noreply.github.com>
2023-04-23 15:52:39 +0000

    Create JavaScript file to create 8 pairs of colours and then select and remove one randomly from the list when run

---
+++ assets/script/card-sort-script.js
@@ -0,0 +1,33 @@
+// create pairs of colors to hopefully eventually be replaced with images //
+
+let colors = [
+    '#33ff33',
+    '#33ff33',
+    '#ff944d',
+    '#ff944d',
+    '#80ccff',
+    '#80ccff',
+    '#ffff66',
+    '#ffff66',
+    '#ff4dff',
+    '#ff4dff',
+    '#ff1a1a',
+    '#ff1a1a',
+    '#dddddd',
+    '#dddddd',
+    '#000099',
+    '#000099',
+];
+
+function selectColor() {
+    // 0-16
+    const random = Math.floor(Math.random() * colors.length)
+    const selected = colors[random];
+    colors.splice(random, 1);
+    return selected;
+}
+
+selectColor()
+selectColor(
+    console.log(colors)
+)
