c9997704ad54d75265f8184c3ade2474c297b093
ajmccredie <>
2023-05-01 18:26:25 +0000

    Start to bug fix collapsing menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 const lineContainer = document.getElementsByClassName("line-container");
+console.log("I have the line container");
 const menuBox = document.getElementsByClassName("menu-box");
 
-lineContainer.addEventListener("click", function() {
-    lineContainer.classList.toggle("active")
-    menuBox.classList.toggle("active")
-   // if (this.classList.contains("active")) {
-     //   this.classList.remove("active");
-     // } else this.classList.add("active");
+lineContainer.addEventListener("click", ()=>{
+  //  lineContainer.classList.toggle("active")
+  //  menuBox.classList.toggle("active")
+    if (this.classList.contains("active")) {
+        this.classList.remove("active");
+        } else this.classList.add("active");
     });
 
 
