4ce0151729f48788783e241e90ba4013fa3a0f6b
ajmccredie <127118760+ajmccredie@users.noreply.github.com>
2023-05-01 11:14:55 +0000

    Format the php email generating code for contact page

--- contact.php
+++ contact.php
@@ -2,6 +2,9 @@
 
 #Receive user input
 $email_address = $_POST['email_address'];
+$full_name = $_POST['full_name'];
+$phone = $_POST['phone'];
+$course = $_POST['course'];
 $feedback = $_POST['feedback'];
 
 #Filter user input
@@ -12,8 +15,14 @@ return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
 $email_address  = filter_email_header($email_address);
 
 #Send email
-$headers = "From: $email_addressn";
-$sent = mail('you@domain.com', 'Feedback Form Submission', $feedback, $headers);
+$headers = "From: $email_address";
+$email_subject = "New Gold Card form submission";
+$email_body = "You have received a new message from $full_name. \n".
+    "Email address: $email_address\n".
+    "Telephone contact: $phone\n".
+    "Interested in: $course\n".
+    "Additional information/queries: $feedback";
+$sent = mail('aj.mccredie@yahoo.co.uk', $email_subject, $email_body, $headers);
 
 #Thank user or notify them of a problem
 if ($sent) {
@@ -24,7 +33,7 @@ if ($sent) {
 </head>
 <body>
 <h1>Thank You</h1>
-<p>Thank you for your feedback.</p>
+<p>Thank you for your interest.</p>
 </body>
 </html>
 <?php
@@ -37,7 +46,7 @@ if ($sent) {
 </head>
 <body>
 <h1>Something went wrong</h1>
-<p>We could not send your feedback. Please try again.</p>
+<p>We could not send your form. Please try again.</p>
 </body>
 </html>
 <?php
